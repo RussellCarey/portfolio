23ca7dec2450461abe5e01e9e93fc6c42e21d38b
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-02-20 18:36:16 +0900

    actions / nginx testing

--- src/components/Pages/styles/project.styles.ts
+++ src/components/Pages/styles/project.styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { theme } from "../../../theme/Theme";
 
 export const Container = styled.div`
   display: flex;
@@ -6,4 +7,6 @@ export const Container = styled.div`
   flex-wrap: wrap;
 
   overflow-y: auto;
+
+  padding: ${theme.spacing.space.medium};
 `;
