bcef2198f89f7a59dc0dd6e9e5f321fd020ff090
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-03-13 14:00:25 +0900

    Drawing api change

--- client/src/components/Canvas/services/dbService.ts
+++ client/src/components/Canvas/services/dbService.ts
@@ -1,9 +1,10 @@
+import isDev from "../../../utils/isDev";
 import axios from "axios";
 
 export const getDrawingData = async () => {
   const req = await axios.request({
     method: "GET",
-    url: "http://localhost:9743/api/drawing/getData",
+    url: isDev() ? "http://localhost:9743/api/drawing/getData" : "/folioapi/drawing/getData",
   });
 
   return req;
@@ -12,7 +13,7 @@ export const getDrawingData = async () => {
 export const uploadDrawing = async (drawingString: string) => {
   const req = await axios.request({
     method: "POST",
-    url: "http://localhost:9743/api/drawing/uploadData",
+    url: isDev() ? "http://localhost:9743/api/drawing/uploadData" : "/folioapi/drawing/uploadData",
     data: {
       drawing: drawingString,
     },
