0fd6ebfccfe462f76b9aafbc2bd7ce1d2122a5cb
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-03-13 14:36:47 +0900

    W

--- server/utils/isDev.ts
+++ server/utils/isDev.ts
@@ -1,5 +1,5 @@
-const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
-console.log("Is dev run and found that the current process in in the mode:" + development);
+const development: boolean = process.env.NODE_ENV === "development";
+console.log("Is dev run and found that development mode is currently running::" + development);
 
 export default function isDev(): boolean {
   return development;
