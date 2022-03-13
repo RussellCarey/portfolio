7660bedb80accb0c2c423c105e445c2d344804a9
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-03-13 14:15:21 +0900

    Drawing api change

--- server/utils/isDev.ts
+++ server/utils/isDev.ts
@@ -1,5 +1,5 @@
-// Check what production were in quickly....
 const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+console.log("Is dev run and found that the current process in in the mode:" + development);
 
 export default function isDev(): boolean {
   return development;
