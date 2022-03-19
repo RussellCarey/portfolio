7e9fae99350975ab57520d2773e8a93840e55b94
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-03-19 17:10:38 +0900

    changed secret

--- server/utils/createServer.ts
+++ server/utils/createServer.ts
@@ -7,6 +7,7 @@ import helmet from "helmet";
 const app = express();
 const cors = require("cors");
 
+//
 const whiteListDev = ["localhost:3000", "http://localhost:3000", "http://127.0.0.1:3000"];
 const whiteListProd = [
   "https://russell-carey.com",
