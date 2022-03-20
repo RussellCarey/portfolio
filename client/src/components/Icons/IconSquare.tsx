5efd994cf806bd5f753b8034a436e1e19ae34ab9
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-03-20 19:37:59 +0900

    added more google anas

--- client/src/components/Icons/IconSquare.tsx
+++ client/src/components/Icons/IconSquare.tsx
@@ -14,6 +14,7 @@ import {
   ThreeDeeSVG,
 } from "./styles/styled";
 
+//
 const IconSquareMain: FunctionComponent<IIconSquareProps> = ({ name, label, themeState, isProject }) => {
   return !isProject ? (
     <IconSquare themeState={themeState}>
